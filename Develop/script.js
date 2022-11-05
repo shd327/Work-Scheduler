03178ad6477c1aad2213baa5fa0be20ad469b48c
Hiro Protagonist <shd327@gmail.com>
2022-11-04 18:17:33 -0600

    added selectors for time ids

--- Develop/script.js
+++ Develop/script.js
@@ -38,3 +38,13 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+$("#hour-9 .description").val(localStorage.getItem('hour-9'))
+$("#hour-10 .description").val(localStorage.getItem('hour-10'))
+$("#hour-11 .description").val(localStorage.getItem('hour-11'))
+$("#hour-12 .description").val(localStorage.getItem('hour-12'))
+$("#hour-13 .description").val(localStorage.getItem('hour-13'))
+$("#hour-14 .description").val(localStorage.getItem('hour-14'))
+$("#hour-15 .description").val(localStorage.getItem('hour-15'))
+$("#hour-16 .description").val(localStorage.getItem('hour-16'))
+$("#hour-17 .description").val(localStorage.getItem('hour-17'))
